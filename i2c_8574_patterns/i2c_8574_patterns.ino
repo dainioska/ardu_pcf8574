c6a89eb5621fd3e02184c7a7add4c6b54103612c
dainioshka <dainioshka@gmail.com>
2020-04-14 11:13:49 +0300

    edit .ino>> patterns HEX and parallel sequence

--- i2c_8574_patterns/i2c_8574_patterns.ino
+++ i2c_8574_patterns/i2c_8574_patterns.ino
@@ -1,11 +1,10 @@
 //Arduino(UNO/Nano)over I2C communication to PCF8574 example
-
 #include <Wire.h>
 
-// second chip PCF8574 I2C address
-
-//Array of patterns
-int datArray[8]={B11111110, B11111101, B11111011, B11110111, B11101111, B11011111, B10111111, B01111111};
+//Array of patterns version "bin"
+//int datArray[8]={B11111110, B11111101, B11111011, B11110111, B11101111, B11011111, B10111111, B01111111};
+//Array of patterns  version "hex"
+int datArray[8]={0xFE,0xFD,0xFB,0xF7,0xEF,0xDF,0xBF,0x7F};
 
 void setup()
 {
@@ -16,29 +15,26 @@ void setup()
 void loop()
 {
 
-for(int i =0; i<8; i++)     //init sequence of patterns from array at first chip PCF8574
+for(int i =0; i<8; i++)     //init parallel sequence of patterns from array at first and second chips PCF8574
 {  
 Wire.beginTransmission(0x27);  //first chip PCF8574 I2C address 27
 Wire.write(datArray[i]);
 Wire.endTransmission();
-Serial.print("Pattern: ");
+Serial.print("Pattern FIRST: ");
 Serial.println(i);
-delay(500);  
-}
-Wire.beginTransmission(0x27);  //reset pattern(FF)
-Wire.write(B11111111);
-Wire.endTransmission();
 
-for(int i =0; i<8; i++)     //init sequence of patterns from array at second chip PCF8574
-{  
-Wire.beginTransmission(0x21);  // second chip PCF8574 I2C address 21
+Wire.beginTransmission(0x21);  //second chip PCF8574 I2C address 21
 Wire.write(datArray[i]);
 Wire.endTransmission();
-Serial.print("Pattern: ");
-Serial.println(i+8);
-delay(500);
+Serial.print("Pattern SECOND: ");
+Serial.println(i);
+
+delay(500);  
 }
-Wire.beginTransmission(0x21);  //reset pattern(FF)
-Wire.write(B11111111);
+Wire.beginTransmission(0x27);  //reset pattern(FF) 
+Wire.write(0xFF);
+Wire.endTransmission();
+Wire.beginTransmission(0x21);  //reset pattern(FF) 
+Wire.write(0xFF);
 Wire.endTransmission();
 }
