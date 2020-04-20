e34e9b06a9952a42d3a3cd15a824c4eb1a2763ec
dainioshka <dainioshka@gmail.com>
2020-04-20 09:29:45 +0300

    add millis()>> arduino_every_master.ino

--- i2c_8574_patterns/i2c_8574_patterns.ino
+++ i2c_8574_patterns/i2c_8574_patterns.ino
@@ -1,40 +1,70 @@
 //Arduino(UNO/Nano)over I2C communication to PCF8574 example
+//how to use millis() and class TimeOut_I2C example(2020-04-19)
+//
 #include <Wire.h>
 
-//Array of patterns version "bin"
-//int datArray[8]={B11111110, B11111101, B11111011, B11110111, B11101111, B11011111, B10111111, B01111111};
-//Array of patterns  version "hex"
-int datArray[8]={0xFE,0xFD,0xFB,0xF7,0xEF,0xDF,0xBF,0x7F};
-
-void setup()
+class TimeOut
 {
+  long OnTime;    //time selected pin"On"
+  long OffTime;   //time selected pin"Off"
+  int pinState;
+  unsigned long prevMillis;
+  
+  int i=0;
+  //Array of patterns (hex)
+  const int datArray[8]={0xFE,0xFD,0xFB,0xF7,0xEF,0xDF,0xBF,0x7F};
+
+public:
+TimeOut(long on, long off)   //init constructor
+{ 
+  OnTime = on;
+  OffTime = off;
+  pinState =LOW;
+  prevMillis = 0;
+}
+
+void Update()
+{
+  unsigned long currentMillis = millis(); 
+  if((pinState == HIGH) && (currentMillis -prevMillis >= OnTime))
+  {
+    pinState = LOW;
+    prevMillis = currentMillis;  
+    Wire.beginTransmission(0x27); 
+    Wire.write(0xFF); 
+    Wire.endTransmission();
+  }
+  else if((pinState ==LOW) && (currentMillis -prevMillis >= OffTime))
+  {
+    pinState = HIGH;
+    prevMillis = currentMillis;
+    if(i < 8)
+    { 
+    Wire.beginTransmission(0x27); 
+    Wire.write(datArray[i]); 
+    Wire.endTransmission();
+    i++;
+    }
+    else
+    {
+      i =0;
+    }   
+  }    
+ }
+};
+
+ TimeOut patternsH(1000, 5000);   //class instance ( HIGH timeout(milisec.) ,LOW timeout(milisec.))
+
+void setup() {
   Serial.begin(9600);
-  Wire.begin();
+  Wire.begin();  
+  Wire.beginTransmission(0x27);  //setup port 27(hex)
+  Wire.write(0xFF);              //reset all pins
+  Wire.endTransmission();
+  delay(2000);
 }
 
-void loop()
-{
+void loop() {
+ patternsH.Update();
 
-for(int i =0; i<8; i++)     //init parallel sequence of patterns from array at first and second chips PCF8574
-{  
-Wire.beginTransmission(0x27);  //first chip PCF8574 I2C address 27
-Wire.write(datArray[i]);
-Wire.endTransmission();
-Serial.print("Pattern FIRST: ");
-Serial.println(i);
-
-Wire.beginTransmission(0x21);  //second chip PCF8574 I2C address 21
-Wire.write(datArray[i]);
-Wire.endTransmission();
-Serial.print("Pattern SECOND: ");
-Serial.println(i);
-
-delay(500);  
-}
-Wire.beginTransmission(0x27);  //reset pattern(FF) 
-Wire.write(0xFF);
-Wire.endTransmission();
-Wire.beginTransmission(0x21);  //reset pattern(FF) 
-Wire.write(0xFF);
-Wire.endTransmission();
 }
