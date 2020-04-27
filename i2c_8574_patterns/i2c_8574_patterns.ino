49781890221f868e0cf82b3e0bebce95b7bbfbbf
dainioshka <dainioshka@gmail.com>
2020-04-27 16:56:42 +0300

    add serial and LCD >> arduino_every_master.ino

--- i2c_8574_patterns/i2c_8574_patterns.ino
+++ i2c_8574_patterns/i2c_8574_patterns.ino
@@ -1,7 +1,12 @@
 //Arduino(UNO/Nano)over I2C communication to PCF8574 example
-//how to use millis() and class TimeOut_I2C example(2020-04-19)
-//
+//How to use long time period (from 1 to 60sec.) millis() and class TimeOut_I2C
+//Function millis() results serial printout and display on LCD(16x4)
+//VERSION(2020-04-27)tested
+
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  //LCD_4 line port 27
 
 class TimeOut
 {
@@ -25,46 +30,68 @@ TimeOut(long on, long off)   //init constructor
 
 void Update()
 {
-  unsigned long currentMillis = millis(); 
+  unsigned long currentMillis = millis()+8000; 
   if((pinState == HIGH) && (currentMillis -prevMillis >= OnTime))
   {
     pinState = LOW;
-    prevMillis = currentMillis;  
-    Wire.beginTransmission(0x27); 
+    prevMillis = currentMillis; 
+    Serial.print("  OnTime:");
+    Serial.println(prevMillis);
+   
+    lcd.setCursor(0,1);         // data display on LCD
+    lcd.print("TimeOn: ");
+    lcd.print(prevMillis); 
+
+    Wire.beginTransmission(0x22); 
     Wire.write(0xFF); 
     Wire.endTransmission();
+   
   }
   else if((pinState ==LOW) && (currentMillis -prevMillis >= OffTime))
   {
     pinState = HIGH;
     prevMillis = currentMillis;
-    if(i < 8)
-    { 
-    Wire.beginTransmission(0x27); 
+    Serial.print("Offtime:");
+    Serial.print(prevMillis);
+    Serial.print("  print i:");
+    Serial.print(i);
+
+    lcd.setCursor(0,2);       // data display on LCD
+    lcd.print("TimeOff: ");
+    lcd.print(prevMillis); 
+    
+    Wire.beginTransmission(0x22); 
     Wire.write(datArray[i]); 
     Wire.endTransmission();
-    i++;
+    if(i < 7)
+    {
+      i++;
     }
     else
     {
-      i =0;
-    }   
+      i=0;
+    } 
   }    
  }
 };
 
- TimeOut patternsH(1000, 5000);   //class instance ( HIGH timeout(milisec.) ,LOW timeout(milisec.))
+ TimeOut patternsH(3000, 10000);   //class instance ( HIGH timeout(milisec.) ,LOW timeout(milisec.))
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("PCF8574 begin >>>");
   Wire.begin();  
-  Wire.beginTransmission(0x27);  //setup port 27(hex)
+  Wire.beginTransmission(0x22);  //setup port 22(hex)
   Wire.write(0xFF);              //reset all pins
   Wire.endTransmission();
+  
+  lcd.init();
+  lcd.backlight();
+  lcd.print("PCF8574 begin >>>");
+   
   delay(2000);
 }
 
 void loop() {
  patternsH.Update();
-
 }
